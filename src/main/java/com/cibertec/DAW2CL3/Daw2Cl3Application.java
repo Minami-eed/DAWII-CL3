d9a9b30b0a090dde632b1700fc14a2b3345a9824
Minami <106195391+Minami-eed@users.noreply.github.com>
2023-11-29 23:59:36 -0500

    Create new project

---
+++ src/main/java/com/cibertec/DAW2CL3/Daw2Cl3Application.java
@@ -0,0 +1,13 @@
+package com.cibertec.DAW2CL3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daw2Cl3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Daw2Cl3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibertec/DAW2CL3/Daw2Cl3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.DAW2CL3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Daw2Cl3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
